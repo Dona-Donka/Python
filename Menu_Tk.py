9b71b460613ed9fb3df8d8f0f8ced8a427d3ed7d
Dominika Maślanka <dominika.maslanka@outlook.com>
2018-10-03 09:31:52 +0200

    Function that open new window added

--- Menu_Tk.py
+++ Menu_Tk.py
@@ -1,5 +1,5 @@
 from Tkinter import *
-#from Tk_RockPaperScissors import RockPaperWindow
+from Tk_RockPaperScissors import RockPaperWindow
 
 
 class Menu(Frame):
@@ -12,28 +12,37 @@ class Menu(Frame):
         exit()
 
     def rockPaperScissorsRun(self):
-        #self.rockPaperWindow = RockPaperWindow()
-        #return self.rockPaperWindow.run()
-        pass
+       #self.objcRock = RockPaperWindow(self)
+        #self.objcRock.initWindow()
+        # self.initWindow().run()
+        root = Tk()
+        root.title("ROCK-PAPER-SCISSORS")
+        # root.configure(background="red")
+        app = RockPaperWindow(root)
+        root.mainloop()
 
     def initWindow(self):
-
         self.master.title("GAMES BOARD")
         self.pack(fill=BOTH, expand=1)
         self.configure(background="sea green", relief="ridge")
 
-        self.helloLabel = Label(self, text = 'Hello in Games Board!', bg="sea green", font=("Comic Sans MS", 18, "bold"), fg="floral white")
+        self.helloLabel = Label(self, text='Hello in Games Board!', bg="sea green", font=("Comic Sans MS", 18, "bold"),
+                                fg="floral white")
         self.helloLabel.grid(row=3, column=5)
 
-        self.button1 = Button(self, text="Rock, Paper, Sissors", bg="sea green", font=("Comic Sans MS", 10), fg="floral white")
-        self.button1.configure(command=lambda: self.rockPaperScissorsRun())
+        self.button1 = Button(self, text="Rock, Paper, Sissors", bg="sea green", font=("Comic Sans MS", 10),
+                              fg="floral white")
+        self.button1.configure(command=self.rockPaperScissorsRun)
         self.button1.grid(row=10, column=0)
 
-        self.button2 = Button(self, text="Guess the number", bg="sea green", font=("Comic Sans MS", 10), fg="floral white")
+        self.button2 = Button(self, text="Guess the number", bg="sea green", font=("Comic Sans MS", 10),
+                              fg="floral white")
         self.button2.grid(row=10, column=12)
 
-root = Tk()
-root.title("GAMES BOARD")
-app = Menu(root)
-root.mainloop()
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("GAMES BOARD")
+    app = Menu(root)
+    root.mainloop()
 
